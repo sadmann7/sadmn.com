f4b74214052fcb7833b93681104584626685b062
sadmann7 <sadmann898@gmail.com>
2024-02-24 07:30:14 +0600

    refactor: twitter -> x

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { ModeToggle } from "@/components/layouts/mode-toggle"
 
@@ -15,36 +14,26 @@ export function SiteHeader() {
           <span className="sr-only">Home</span>
         </Link>
         <nav className="flex items-center space-x-1">
-          <Link
-            href={siteConfig.links.githubProfile}
-            target="_blank"
-            rel="noreferrer"
-            className={cn(
-              buttonVariants({
-                variant: "ghost",
-                size: "icon",
-                className: "h-7 w-7",
-              })
-            )}
-          >
-            <Icons.gitHub className="size-4" />
-            <span className="sr-only">GitHub</span>
-          </Link>
-          <Link
-            href={siteConfig.links.twitter}
-            target="_blank"
-            rel="noreferrer"
-            className={cn(
-              buttonVariants({
-                variant: "ghost",
-                size: "icon",
-                className: "h-7 w-7",
-              })
-            )}
-          >
-            <Icons.twitter className="size-3 fill-current" />
-            <span className="sr-only">Twitter</span>
-          </Link>
+          <Button variant="ghost" size="icon" className="size-7" asChild>
+            <Link
+              href={siteConfig.links.githubProfile}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icons.gitHub className="size-4" />
+              <span className="sr-only">GitHub</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" size="icon" className="size-7">
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icons.x className="size-3 fill-current" />
+              <span className="sr-only">X</span>
+            </Link>
+          </Button>
           <ModeToggle />
         </nav>
       </div>
